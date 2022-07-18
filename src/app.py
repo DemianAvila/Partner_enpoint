8b132a2093cee125c4e54209e5f23f8f68721164
demian_avila <demian_avila@comunidad.unam.mx>
2022-07-18 11:13:37 -0500

    Manual validation of fields

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from odooly import Client
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response, status
 
 
 app = FastAPI()
@@ -11,45 +11,91 @@ client = Client(server = "http://localhost:8069",
     password = "admin"
 )
 
+
+
 @app.get('/get_routes')
-async def get_routes(request: Request):
+async def get_routes(request: Request, 
+    response: Response):
     #TRY TO GET THE REQUEST BODY
     try:
         req = await request.json()
     except Exception as e:
+        response.status_code = status.HTTP_408_REQUEST_TIMEOUT
         return {
-            "STATUS": 408,
             "DESCRIPTION": "Timeout, could not get the request",
             "EXCEPTION": str(e)
         }
     #VALIDATE THE REQUEST
     accepted_parameters = ['filters', 'order_by', 'accept']
+    accepted_filters = ['route_name', 'route_id', 'partner_name', 'parter_id',
+            'available', 'active']
+    order_by_params = ['name', 'zone', 'code', 'distance', 'partner', 
+            'start_date', 'end_date']
+    accept_search = ['case_sensitive', 'reg_exp']
     for key in req.keys():
         if key not in accepted_parameters:
+            response.status_code = status.HTTP_404_NOT_FOUND
             return {
-                "STATUS": 404,
-                "DESCRIPTION": f"{key} is not accepted in this request"
+                "DESCRIPTION": f"'{key}' is not accepted in this request"
             }
+        else:
+            if key == 'filters':
+                if str(type(req[key])) != '<class \'dict\'>':
+                    response.status_code = status.HTTP_400_BAD_REQUEST
+                    return {
+                        "DESCRIPTION": f"'{key}' must be object"
+                    }
+                for subkey in req[key].keys():
+                    if subkey not in accepted_filters:
+                        response.status_code = status.HTTP_404_NOT_FOUND
+                        return {
+                            "DESCRIPTION": f"Subkey '{subkey}' not accepted in '{key}'"
+                        }
+            elif key == 'order_by':
+                if str(type(req[key])) != '<class \'dict\'>':
+                    response.status_code = status.HTTP_400_BAD_REQUEST
+                    return {
+                        "DESCRIPTION": f"'{key}' must be object"
+                    }
+                for subkey in req[key].keys():
+                    if subkey not in order_by_params:
+                        response.status_code = status.HTTP_404_NOT_FOUND
+                        return {
+                            "DESCRIPTION": f"Subkey '{subkey}' not accepted in '{key}'"
+                        }
+            elif key == 'accept':
+                if str(type(req[key])) != '<class \'dict\'>':
+                    response.status_code = status.HTTP_400_BAD_REQUEST
+                    return {
+                        "DESCRIPTION": f"'{key}' must be object"
+                    }
+                for subkey in req[key].keys():
+                    if subkey not in accept_search:
+                        response.status_code = status.HTTP_404_NOT_FOUND
+                        return {
+                            "DESCRIPTION": f"Subkey '{subkey}' not accepted in '{key}'"
+                        }
+
     #TRY TO READ THE ODOO ROUTE'S MODEL
     try:        
         routes = client.env['trx_bus_traxi.routes'].search([])
     except Exception as e:
+        response.status_code = status.HTTP_408_REQUEST_TIMEOUT
         return {
-            "STATUS": 408,
             "DESCRIPTION": "Timeout, no connection to database",
             "EXCEPTION": str(e)
         }
-
-    response = {
-        "STATUS": 200,
+    
+    response.status_code = status.HTTP_200_OK
+    resp = {
         "DESCRIPTION": "OK",
         "routes":[]
     }
 
     for route in routes:
-        response['routes'].append({
+        resp['routes'].append({
                 "name": route.name
             }
         )
-    return response
+    return resp
 
